eea2d8af5e5c104dc6027e14a00c2b4f2f498598
DamianTwarog99 <81587960+DamianTwarog99@users.noreply.github.com>
2021-07-18 17:09:19 +0200

    Add new files

---
+++ Damian Twaróg/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿
+namespace Damian_Twaróg
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AX1 = new System.Windows.Forms.TextBox();
+            this.BX1 = new System.Windows.Forms.TextBox();
+            this.CX1 = new System.Windows.Forms.TextBox();
+            this.DX1 = new System.Windows.Forms.TextBox();
+            this.AX2 = new System.Windows.Forms.TextBox();
+            this.BX2 = new System.Windows.Forms.TextBox();
+            this.CX2 = new System.Windows.Forms.TextBox();
+            this.DX2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.AXR1 = new System.Windows.Forms.RadioButton();
+            this.BXR1 = new System.Windows.Forms.RadioButton();
+            this.CXR1 = new System.Windows.Forms.RadioButton();
+            this.DXR1 = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.DXR2 = new System.Windows.Forms.RadioButton();
+            this.CXR2 = new System.Windows.Forms.RadioButton();
+            this.BXR2 = new System.Windows.Forms.RadioButton();
+            this.AXR2 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(8, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AX";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(8, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "BX";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(8, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CX";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(8, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DX";
+            // 
+            // AX1
+            // 
+            this.AX1.Location = new System.Drawing.Point(34, 55);
+            this.AX1.Name = "AX1";
+            this.AX1.ReadOnly = true;
+            this.AX1.Size = new System.Drawing.Size(100, 23);
+            this.AX1.TabIndex = 4;
+            this.AX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BX1
+            // 
+            this.BX1.Location = new System.Drawing.Point(34, 84);
+            this.BX1.Name = "BX1";
+            this.BX1.ReadOnly = true;
+            this.BX1.Size = new System.Drawing.Size(100, 23);
+            this.BX1.TabIndex = 5;
+            this.BX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // CX1
+            // 
+            this.CX1.Location = new System.Drawing.Point(34, 113);
+            this.CX1.Name = "CX1";
+            this.CX1.ReadOnly = true;
+            this.CX1.Size = new System.Drawing.Size(100, 23);
+            this.CX1.TabIndex = 6;
+            this.CX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // DX1
+            // 
+            this.DX1.Location = new System.Drawing.Point(34, 142);
+            this.DX1.Name = "DX1";
+            this.DX1.ReadOnly = true;
+            this.DX1.Size = new System.Drawing.Size(100, 23);
+            this.DX1.TabIndex = 7;
+            this.DX1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // AX2
+            // 
+            this.AX2.Location = new System.Drawing.Point(140, 55);
+            this.AX2.Name = "AX2";
+            this.AX2.Size = new System.Drawing.Size(100, 23);
+            this.AX2.TabIndex = 8;
+            this.AX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.AX2.TextChanged += new System.EventHandler(this.AX2_TextChanged);
+            // 
+            // BX2
+            // 
+            this.BX2.Location = new System.Drawing.Point(140, 84);
+            this.BX2.Name = "BX2";
+            this.BX2.Size = new System.Drawing.Size(100, 23);
+            this.BX2.TabIndex = 9;
+            this.BX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.BX2.TextChanged += new System.EventHandler(this.BX2_TextChanged);
+            // 
+            // CX2
+            // 
+            this.CX2.Location = new System.Drawing.Point(140, 113);
+            this.CX2.Name = "CX2";
+            this.CX2.Size = new System.Drawing.Size(100, 23);
+            this.CX2.TabIndex = 10;
+            this.CX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.CX2.TextChanged += new System.EventHandler(this.CX2_TextChanged);
+            // 
+            // DX2
+            // 
+            this.DX2.Location = new System.Drawing.Point(140, 142);
+            this.DX2.Name = "DX2";
+            this.DX2.Size = new System.Drawing.Size(100, 23);
+            this.DX2.TabIndex = 11;
+            this.DX2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.DX2.TextChanged += new System.EventHandler(this.DX2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(34, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(140, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Random";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.DXR1);
+            this.groupBox1.Controls.Add(this.CXR1);
+            this.groupBox1.Controls.Add(this.BXR1);
+            this.groupBox1.Controls.Add(this.AXR1);
+            this.groupBox1.Location = new System.Drawing.Point(34, 175);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(100, 132);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Z";
+            // 
+            // AXR1
+            // 
+            this.AXR1.AutoSize = true;
+            this.AXR1.Location = new System.Drawing.Point(3, 19);
+            this.AXR1.Name = "AXR1";
+            this.AXR1.Size = new System.Drawing.Size(40, 19);
+            this.AXR1.TabIndex = 0;
+            this.AXR1.TabStop = true;
+            this.AXR1.Text = "AX";
+            this.AXR1.UseVisualStyleBackColor = true;
+            // 
+            // BXR1
+            // 
+            this.BXR1.AutoSize = true;
+            this.BXR1.Location = new System.Drawing.Point(3, 44);
+            this.BXR1.Name = "BXR1";
+            this.BXR1.Size = new System.Drawing.Size(39, 19);
+            this.BXR1.TabIndex = 1;
+            this.BXR1.TabStop = true;
+            this.BXR1.Text = "BX";
+            this.BXR1.UseVisualStyleBackColor = true;
+            // 
+            // CXR1
+            // 
+            this.CXR1.AutoSize = true;
+            this.CXR1.Location = new System.Drawing.Point(3, 69);
+            this.CXR1.Name = "CXR1";
+            this.CXR1.Size = new System.Drawing.Size(40, 19);
+            this.CXR1.TabIndex = 2;
+            this.CXR1.TabStop = true;
+            this.CXR1.Text = "CX";
+            this.CXR1.UseVisualStyleBackColor = true;
+            // 
+            // DXR1
+            // 
+            this.DXR1.AutoSize = true;
+            this.DXR1.Location = new System.Drawing.Point(3, 94);
+            this.DXR1.Name = "DXR1";
+            this.DXR1.Size = new System.Drawing.Size(40, 19);
+            this.DXR1.TabIndex = 3;
+            this.DXR1.TabStop = true;
+            this.DXR1.Text = "DX";
+            this.DXR1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.DXR2);
+            this.groupBox2.Controls.Add(this.CXR2);
+            this.groupBox2.Controls.Add(this.BXR2);
+            this.groupBox2.Controls.Add(this.AXR2);
+            this.groupBox2.Location = new System.Drawing.Point(140, 175);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(100, 132);
+            this.groupBox2.TabIndex = 15;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Do";
+            // 
+            // DXR2
+            // 
+            this.DXR2.AutoSize = true;
+            this.DXR2.Location = new System.Drawing.Point(3, 94);
+            this.DXR2.Name = "DXR2";
+            this.DXR2.Size = new System.Drawing.Size(40, 19);
+            this.DXR2.TabIndex = 3;
+            this.DXR2.TabStop = true;
+            this.DXR2.Text = "DX";
+            this.DXR2.UseVisualStyleBackColor = true;
+            // 
+            // CXR2
+            // 
+            this.CXR2.AutoSize = true;
+            this.CXR2.Location = new System.Drawing.Point(3, 69);
+            this.CXR2.Name = "CXR2";
+            this.CXR2.Size = new System.Drawing.Size(40, 19);
+            this.CXR2.TabIndex = 2;
+            this.CXR2.TabStop = true;
+            this.CXR2.Text = "CX";
+            this.CXR2.UseVisualStyleBackColor = true;
+            // 
+            // BXR2
+            // 
+            this.BXR2.AutoSize = true;
+            this.BXR2.Location = new System.Drawing.Point(3, 44);
+            this.BXR2.Name = "BXR2";
+            this.BXR2.Size = new System.Drawing.Size(39, 19);
+            this.BXR2.TabIndex = 1;
+            this.BXR2.TabStop = true;
+            this.BXR2.Text = "BX";
+            this.BXR2.UseVisualStyleBackColor = true;
+            // 
+            // AXR2
+            // 
+            this.AXR2.AutoSize = true;
+            this.AXR2.Location = new System.Drawing.Point(3, 19);
+            this.AXR2.Name = "AXR2";
+            this.AXR2.Size = new System.Drawing.Size(40, 19);
+            this.AXR2.TabIndex = 0;
+            this.AXR2.TabStop = true;
+            this.AXR2.Text = "AX";
+            this.AXR2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(34, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(206, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "MOV";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(34, 342);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(206, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "XCHG";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 378);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DX2);
+            this.Controls.Add(this.CX2);
+            this.Controls.Add(this.BX2);
+            this.Controls.Add(this.AX2);
+            this.Controls.Add(this.DX1);
+            this.Controls.Add(this.CX1);
+            this.Controls.Add(this.BX1);
+            this.Controls.Add(this.AX1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Emulator Intel 8086";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AX1;
+        private System.Windows.Forms.TextBox BX1;
+        private System.Windows.Forms.TextBox CX1;
+        private System.Windows.Forms.TextBox DX1;
+        private System.Windows.Forms.TextBox AX2;
+        private System.Windows.Forms.TextBox BX2;
+        private System.Windows.Forms.TextBox CX2;
+        private System.Windows.Forms.TextBox DX2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton DXR1;
+        private System.Windows.Forms.RadioButton CXR1;
+        private System.Windows.Forms.RadioButton BXR1;
+        private System.Windows.Forms.RadioButton AXR1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton DXR2;
+        private System.Windows.Forms.RadioButton CXR2;
+        private System.Windows.Forms.RadioButton BXR2;
+        private System.Windows.Forms.RadioButton AXR2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Damian Twaróg/Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Damian_Twaróg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AX2_TextChanged(object sender, EventArgs e)
+        {
+            AX1.Text = AX2.Text;
+            AX2.MaxLength = 4;
+        }
+
+        private void BX2_TextChanged(object sender, EventArgs e)
+        {
+            BX1.Text = BX2.Text;
+            BX2.MaxLength = 4;
+        }
+
+        private void CX2_TextChanged(object sender, EventArgs e)
+        {
+            CX1.Text = CX2.Text;
+            CX2.MaxLength = 4;
+        }
+
+        private void DX2_TextChanged(object sender, EventArgs e)
+        {
+            DX1.Text = DX2.Text;
+            DX2.MaxLength = 4;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            AX1.Text = "0000";
+            BX1.Text = "0000";
+            CX1.Text = "0000";
+            DX1.Text = "0000";
+            AX2.Text = "0000";
+            BX2.Text = "0000";
+            CX2.Text = "0000";
+            DX2.Text = "0000";
+        }
+        public static void XCHG(ref string x, ref string y)
+        {
+            string tmp = x;
+            x = y;
+            y = tmp;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var random = new Random();
+            string AX = "";
+            string BX = "";
+            string CX = "";
+            string DX = "";
+            for (int i = 0; i < 4; i++)
+            {
+                int A = random.Next(0, 16);
+                AX += A.ToString("X");
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int B = random.Next(0, 16);
+                BX += B.ToString("X");
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int C = random.Next(0, 16);
+                CX += C.ToString("X");
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                int D = random.Next(0, 16);
+                DX += D.ToString("X");
+            }
+
+
+            AX1.Text = AX;
+            AX2.Text = AX;
+            BX1.Text = BX;
+            BX2.Text = BX;
+            CX1.Text = CX;
+            CX2.Text = CX;
+            DX1.Text = DX;
+            DX2.Text = DX;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (AXR1.Checked)
+            {
+
+                if (BXR2.Checked)
+                {
+                    BX1.Text = AX1.Text;
+                }
+                else if (CXR2.Checked)
+                {
+                    CX1.Text = AX1.Text;
+                }
+                else if (DXR2.Checked)
+                {
+                    DX1.Text = AX1.Text;
+                }
+            }
+            if (BXR1.Checked)
+            {
+                if (AXR2.Checked)
+                {
+                    AX1.Text = BX1.Text;
+                }
+
+                else if (CXR2.Checked)
+                {
+                    CX1.Text = BX1.Text;
+                }
+                else if (DXR2.Checked)
+                {
+                    DX1.Text = BX1.Text;
+                }
+            }
+            if (CXR1.Checked)
+            {
+                if (AXR2.Checked)
+                {
+                    AX1.Text = CX1.Text;
+                }
+                else if (BXR2.Checked)
+                {
+                    BX1.Text = CX1.Text;
+                }
+
+                else if (DXR2.Checked)
+                {
+                    DX1.Text = CX1.Text;
+                }
+            }
+            if (DXR1.Checked)
+            {
+                if (AXR2.Checked)
+                {
+                    AX1.Text = DX1.Text;
+                }
+                else if (BXR2.Checked)
+                {
+                    BX1.Text = DX1.Text;
+                }
+
+                else if (CXR2.Checked)
+                {
+                    CX1.Text = DX1.Text;
+                }
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (AXR1.Checked)
+            {
+                if (BXR2.Checked)
+                {
+                    string x = AX1.Text;
+                    string y = BX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    AX1.Text = x;
+                    BX1.Text = y;
+                }
+                else if (CXR2.Checked)
+                {
+                    string x = AX1.Text;
+                    string y = CX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    AX1.Text = x;
+                    CX1.Text = y;
+                }
+                else if (DXR2.Checked)
+                {
+                    string x = AX1.Text;
+                    string y = DX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    AX1.Text = x;
+                    DX1.Text = y;
+                }
+            }
+            if (BXR1.Checked)
+            {
+                if (AXR2.Checked)
+                {
+                    string x = BX1.Text;
+                    string y = AX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    BX1.Text = x;
+                    AX1.Text = y;
+                }
+
+                else if (CXR2.Checked)
+                {
+                    string x = BX1.Text;
+                    string y = CX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    BX1.Text = x;
+                    CX1.Text = y;
+                }
+                else if (DXR2.Checked)
+                {
+                    string x = BX1.Text;
+                    string y = DX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    BX1.Text = x;
+                    DX1.Text = y;
+                }
+            }
+            if (CXR1.Checked)
+            {
+                if (AXR2.Checked)
+                {
+                    string x = CX1.Text;
+                    string y = AX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    CX1.Text = x;
+                    AX1.Text = y;
+                }
+                else if (BXR2.Checked)
+                {
+                    string x = CX1.Text;
+                    string y = BX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    CX1.Text = x;
+                    BX1.Text = y;
+                }
+
+                else if (DXR2.Checked)
+                {
+                    string x = CX1.Text;
+                    string y = DX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    CX1.Text = x;
+                    DX1.Text = y;
+                }
+            }
+            if (DXR1.Checked)
+            {
+                if (AXR2.Checked)
+                {
+                    string x = DX1.Text;
+                    string y = AX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    DX1.Text = x;
+                    AX1.Text = y;
+                }
+                else if (BXR2.Checked)
+                {
+                    string x = DX1.Text;
+                    string y = BX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    DX1.Text = x;
+                    BX1.Text = y;
+                }
+
+                else if (CXR2.Checked)
+                {
+                    string x = DX1.Text;
+                    string y = CX1.Text;
+
+                    XCHG(ref x, ref y);
+
+                    DX1.Text = x;
+                    CX1.Text = y;
+                }
+            }
+
+        }
+    }
+}
+    
+
+    
+    
+
+
